3c035ceb7587327c0efc7bb992790d3a73e49dde
gujinfei <gjfly007@163.com>
2021-10-20 14:55:46 +0800

    ANOTHER FUNCTION

--- main.go
+++ main.go
@@ -6,9 +6,14 @@ import (
 )
 
 func main() {
-	var s, sep string
-	for i := 0; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
+	// var s, sep string
+	// for i := 0; i < len(os.Args); i++ {
+	// 	s += sep + os.Args[i]
+	// 	sep = " "
+	// }
+	s, sep := "", ""
+	for _, arg := range os.Args[0:] {
+		s += sep + arg
 		sep = " "
 	}
 	fmt.Println(s)
